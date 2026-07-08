3f5ebc38952c5c5ba2123f4a7d51df65b1b9e0ea
caitlinmiriam <caitlinmiriam@gmail.com>
2026-07-08 19:42:46 +0530

    Added Solution for LC189

---
+++ RotateArray.java
@@ -0,0 +1,40 @@
+// 189. Rotate Array
+
+// Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+// Example 1:
+// Input: nums = [1,2,3,4,5,6,7], k = 3
+// Output: [5,6,7,1,2,3,4]
+// Explanation:
+// rotate 1 steps to the right: [7,1,2,3,4,5,6]
+// rotate 2 steps to the right: [6,7,1,2,3,4,5]
+// rotate 3 steps to the right: [5,6,7,1,2,3,4]
+  
+// Example 2:
+// Input: nums = [-1,-100,3,99], k = 2
+// Output: [3,99,-1,-100]
+// Explanation: 
+// rotate 1 steps to the right: [99,-1,-100,3]
+// rotate 2 steps to the right: [3,99,-1,-100]
+  
+class Solution {
+    public void rotate(int[] nums, int k) {
+        if(nums.length<=1){
+            return;
+        }
+        int len=nums.length;
+        k=k%len;
+        rotateArray(nums,0,len-1);
+        rotateArray(nums,0,k-1);
+        rotateArray(nums,k,len-1);
+    }
+    private void rotateArray(int arr[],int start,int end){
+        while(start<end){
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+}
