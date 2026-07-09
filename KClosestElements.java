ceb15e222d71ad747ac01fee3ac0575534bdbdc4
caitlinmiriam <caitlinmiriam@gmail.com>
2026-07-09 08:25:53 +0530

    Added Solution for LC658
    Updated comments and examples for clarity.

---
+++ KClosestElements.java
@@ -0,0 +1,51 @@
+// 658. Find K Closest Elements
+
+// Given a sorted integer array arr, two integers k and x, return the k closest integers to x in the array. The result should also be sorted in ascending order.
+// An integer a is closer to x than an integer b if:
+// |a - x| < |b - x|, or
+// |a - x| == |b - x| and a < b
+ 
+// Example 1:
+// Input: arr = [1,2,3,4,5], k = 4, x = 3
+// Output: [1,2,3,4]
+
+// Example 2:
+// Input: arr = [1,1,2,3,4,5], k = 4, x = -1
+// Output: [1,1,2,3]
+  
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        List<Integer> list=new ArrayList<>();
+        int index=arr.length;
+        for (int i=0;i<arr.length;i++) {
+            if (arr[i]>=x){
+                index=i;
+                break;
+            }
+        }
+        int left=index-1;
+        int right=index;
+        while(list.size()<k){
+            if(left<0) {
+                list.add(arr[right]);
+                right++;
+            } 
+            else if(right>=arr.length){
+                list.add(arr[left]);
+                left--;
+            } 
+            else{
+                if(Math.abs(arr[left]-x)<=Math.abs(arr[right]-x)){
+                    list.add(arr[left]);
+                    left--;
+                } 
+                else{
+                    list.add(arr[right]);
+                    right++;
+                }
+            }
+        }
+        Collections.sort(list);
+        return list;
+    }
+}
