1641156c7323734bbe5ca489ba8ce88b74187b58
caitlinmiriam <caitlinmiriam@gmail.com>
2026-07-21 14:48:40 +0530

    Implement decoding logic for encoded messages
    The class implements a solution to decode a string of numbers into possible letter combinations based on a mapping. It includes examples and handles edge cases like leading zeros.

---
+++ DecodingWays.java
@@ -0,0 +1,63 @@
+// 91. Decode Ways
+
+// You have intercepted a secret message encoded as a string of numbers. The message is decoded via the following mapping:
+// "1" -> 'A'
+// "2" -> 'B'
+// ...
+// "25" -> 'Y'
+// "26" -> 'Z'
+// However, while decoding the message, you realize that there are many different ways you can decode the message because some codes are contained in other codes ("2" and "5" vs "25").
+// For example, "11106" can be decoded into:
+// "AAJF" with the grouping (1, 1, 10, 6)
+// "KJF" with the grouping (11, 10, 6)
+// The grouping (1, 11, 06) is invalid because "06" is not a valid code (only "6" is valid).
+// Note: there may be strings that are impossible to decode.
+// Given a string s containing only digits, return the number of ways to decode it. If the entire string cannot be decoded in any valid way, return 0.
+// The test cases are generated so that the answer fits in a 32-bit integer.
+
+// Example 1:
+// Input: s = "12"
+// Output: 2
+// Explanation:
+// "12" could be decoded as "AB" (1 2) or "L" (12).
+
+// Example 2:
+// Input: s = "226"
+// Output: 3
+// Explanation:
+// "226" could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
+
+// Example 3:
+// Input: s = "06"
+// Output: 0
+// Explanation:
+// "06" cannot be mapped to "F" because of the leading zero ("6" is different from "06"). In this case, the string is not a valid encoding, so return 0.
+
+class Solution {
+    public int numDecodings(String s) {
+        int dp[]=new int[s.length()+1];
+        Arrays.fill(dp,-1);
+        return numDecodings(s,0,dp);
+    }
+    public int numDecodings(String s,int i,int dp[]){
+        if(i==s.length()){
+            return 1;
+        }
+        if(s.charAt(i)=='0'){
+            return 0;
+        }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        int ans=numDecodings(s,i+1,dp);
+        dp[i]=ans;
+        if(i+1<s.length()){
+            int temp=(s.charAt(i)-'0')*10+(s.charAt(i+1)-'0');
+            if(temp>=10&&temp<=26){
+                ans+=numDecodings(s,i+2,dp);
+            }
+        }
+        dp[i]=ans;
+        return ans;
+    }
+}
