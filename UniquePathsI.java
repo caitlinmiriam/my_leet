c3a6648824e98a75c2309083a8564da618fe90b5
caitlinmiriam <caitlinmiriam@gmail.com>
2026-07-28 14:00:46 +0530

    Implement recursive method for unique paths
    Added recursive solution for unique paths problem.

--- UniquePathsI.java
+++ UniquePathsI.java
@@ -63,3 +63,23 @@ class Solution {
         return dp[i][j];
     }
 }
+
+// Recursion
+class Solution {
+    public int uniquePaths(int m, int n) {
+        return helper(m-1,n-1);
+    }
+    public int helper(int i,int j){
+        if(i==0&&j==0){
+            return 1;
+        }
+        if(i<0||j<0){
+            return 0;
+        }
+        int up=helper(i-1,j);
+        int left=helper(i,j-1);
+        return up+left;
+    }
+}
+// 38/64
+// TLE
