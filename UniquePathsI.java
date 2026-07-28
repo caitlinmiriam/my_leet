f2cd15e7258a771290d83ffda70efe28c3c27abd
caitlinmiriam <caitlinmiriam@gmail.com>
2026-07-28 13:49:18 +0530

    Implement uniquePaths using memoization
    Added a top-down memoization approach to calculate unique paths.

--- UniquePathsI.java
+++ UniquePathsI.java
@@ -37,3 +37,29 @@ class Solution {
         return dp[m-1][n-1];
     }
 }
+
+// Top-Down (Memoization) 
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int dp[][]=new int[m][n];
+        for(int i=0;i<m;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return uniquePaths(m-1,n-1,dp);
+    }
+    public int uniquePaths(int i,int j,int dp[][]){
+        if(i==0&&j==0){
+            return 1;
+        }
+        if(i<0||j<0){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int up=uniquePaths(i-1,j,dp);
+        int left=uniquePaths(i,j-1,dp);
+        dp[i][j]=up+left;
+        return dp[i][j];
+    }
+}
