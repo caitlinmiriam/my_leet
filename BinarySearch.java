c9c84f5068519140fb92536399e49bc1176c989c
caitlinmiriam <caitlinmiriam@gmail.com>
2026-08-12 19:44:26 +0530

    Implement binary search in Solution class

--- BinarySearch.java
+++ BinarySearch.java
@@ -32,6 +32,27 @@ class Solution {
         }
         return -1;
     }
-// }
+}
 // TC: O(log n)
 // SC: O(1)
+
+class Solution {
+    public int search(int[] nums, int target) {
+        return search(nums,0,nums.length-1,target);
+    }
+    public int search(int nums[],int low,int high,int target){
+        if(low>high){
+            return -1;
+        }
+        int mid=low+(high-low)/2;
+        if(nums[mid]==target){
+            return mid;
+        }
+        else if(target>nums[mid]){
+            return search(nums,mid+1,high,target);
+        }
+        return search(nums,low,mid-1,target);
+    }
+}
+// TC: O(log n)
+// SC: O(log n)
