e7d08f64197cb61bda8c3944d648a5209e43be6b
caitlinmiriam <caitlinmiriam@gmail.com>
2026-08-14 14:27:46 +0530

    Implement maximum length substring with two occurrences
    Refactored the implementation of the maximum length substring function to maintain at most two occurrences of each character.

---
+++ (DC14082026) MaxLenSubstringWithTwoMaxOccurrences.java
@@ -0,0 +1,35 @@
+// 3090. Maximum Length Substring With Two Occurrences
+
+// Given a string s, return the maximum length of a substring such that it contains at most two occurrences of each character.
+ 
+// Example 1:
+// Input: s = "bcbbbcba"
+// Output: 4
+// Explanation:
+// The following substring has a length of 4 and contains at most two occurrences of each character: "bcbbbcba".
+
+// Example 2:
+// Input: s = "aaaa"
+// Output: 2
+// Explanation:
+// The following substring has a length of 2 and contains at most two occurrences of each character: "aaaa".
+
+class Solution {
+    public int maximumLengthSubstring(String s) {
+        int freq[]=new int[26];
+        int left=0;
+        int maxLen=0;
+        for(int right=0;right<s.length();right++){
+            freq[s.charAt(right)-'a']++;
+            while(freq[s.charAt(right)-'a']>2){
+                freq[s.charAt(left)-'a']--;
+                left++;
+            }
+            maxLen=Math.max(maxLen,right-left+1);
+        }
+        return maxLen;
+    }
+}
+
+// TC: O(n)
+// SC: O(1)
